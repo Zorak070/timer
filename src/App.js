b54470ef5d81e2b60b2bedf1a5960402a1ecc681
zorak070 <s.sucil1234@gmail.com>
2020-01-26 17:11:53 +0545

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Timer from './containers/Timer';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App d-flex  flex-column container">
+        <div className="Header">
+          <h1>Web Timer App</h1>
+        </div>
+
+        <div className="d-flex flex-fill justify-content-center flex-column">
+          <div className="row">
+            <div className="col-md-6 offset-md-3" >
+              <Timer />
+            </div>
+          </div>
+        </div>
+        <div className="d-flex flex-grow-1" />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/containers/Timer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Timer.css';
+
+class Timer extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="Timer">
+                <h2>Timer</h2>
+            </div>
+        );
+    }
+}
+
+export default Timer;
